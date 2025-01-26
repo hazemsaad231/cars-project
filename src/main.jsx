6617798931a654ba0069ffd936dc0a0ddbdfafd7
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-01-26 18:14:43 +0200

    rrr

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContextProvider from './components/context/context'
+import ContextProvider from './components/context/Context'
 
 
 
