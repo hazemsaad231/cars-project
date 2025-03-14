bbcde846194e691c44b349f02fa7270a5fe8e673
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-03-14 20:16:35 +0200

    new

--- src/components/login/image.jsx
+++ src/components/login/image.jsx
@@ -7,7 +7,7 @@ const Image = () => {
         
 <div className='w-full h-screen'>
     <video autoPlay loop muted disablePictureInPicture playsinline className='w-full h-screen object-cover'>
-        {/* <source src={newImage} type="video/mp4" /> */}
+        <source src={newImage} type="video/mp4" />
     </video>
 </div>
     )
