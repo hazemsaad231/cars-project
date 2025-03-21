d361a422c79fbc9b1693fd0d9f2a60257cd41ae9
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-03-21 21:19:37 +0200

    push

--- src/components/Home/part1.jsx
+++ src/components/Home/part1.jsx
@@ -16,12 +16,12 @@ const One = () => {
                             a car <span className='text-blue-500'>Easily</span>
                         </div>
                     </h1>
-                    <p className='mt-4 font-serif w-[90%] m-auto text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl'>
+                    <p className='mt-4 font-serif w-[80%] m-auto text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl'>
                         Get a car wherever and whenever you need it with your iOS and Android.
                     </p>
                 </div>
                 <div className='pt-10 sm:pt-10 md:pt-24 lg:pt-20 xl:pt-10'>
-                    <img src={bg} alt="bg" id='bg' loading='lazy'/>
+                    <img src={bg} alt="bg" id='bg' decoding="async"/>
                 </div>
             </div>
         </>
