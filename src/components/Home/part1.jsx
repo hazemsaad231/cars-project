36fcea8afe6daf43ab529463db2043b75fa777e3
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-03-22 19:54:29 +0200

    done

--- src/components/Home/part1.jsx
+++ src/components/Home/part1.jsx
@@ -1,4 +1,4 @@
-import bg from '../../assets/img/car.webp';
+
 const One = () => {
 
    
@@ -16,12 +16,12 @@ const One = () => {
                             a car <span className='text-blue-500'>Easily</span>
                         </div>
                     </h1>
-                    <p className='mt-4 font-serif w-[80%] m-auto text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl'>
+                    <p className='mt-4 font-serif w-[75%] m-auto text-sm sm:text-sm md:text-base lg:text-lg xl:text-xl'>
                         Get a car wherever and whenever you need it with your iOS and Android.
                     </p>
                 </div>
                 <div className='pt-10 sm:pt-10 md:pt-24 lg:pt-20 xl:pt-10'>
-                    <img src={bg} alt="bg" id='bg'/>
+                    <img src='/car.webp' alt="bg" id='bg'/>
                 </div>
             </div>
         </>
