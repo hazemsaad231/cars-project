ffd3add91b088679054569e8f8da0e2e279dea55
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-03-25 00:04:17 +0200

    gone

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,30 @@
-import One from './part1';
-import Two from './part2';
-import Four from './part4'
-import Six from './part6'
-import Seven from './part7';
-import Eight from './part8';
-import Offer from './part3';
-import Brand from './part5';
+// import One from './part1';
+// import Two from './part2';
+// import Four from './part4'
+// import Six from './part6'
+// import Seven from './part7';
+// import Eight from './part8';
+// import Brand from './part5';
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import Aos from "aos";
 import "aos/dist/aos.css";
+import React, { Suspense } from "react";
+
+const One = React.lazy(() => import("./part1"));
+const Two = React.lazy(() => import("./part2"));
+const Offer = React.lazy(() => import("./part3"));
+const Four = React.lazy(() => import("./part4"));
+const Brand = React.lazy(() => import("./part5"));
+const Six = React.lazy(() => import("./part6"));
+const Seven = React.lazy(() => import("./part7"));
+const Eight = React.lazy(() => import("./part8"));
+
+
+
+
+
 
 const Home = ()=>{
 
@@ -42,6 +56,7 @@ const Home = ()=>{
 <>
 <ToastContainer/>
 <div style={{ fontFamily: "Arial" }}>
+<Suspense fallback = ''>
 <One/>
 <Two/>
 <Offer/>
@@ -50,6 +65,7 @@ const Home = ()=>{
 <Six/>
 <Seven/>
 <Eight/>
+</Suspense>
 </div>
 
        
--- src/components/Home/part5.jsx
+++ src/components/Home/part5.jsx
@@ -3,14 +3,10 @@ import { Brands } from "./data";
 const Brand = () => {
 
 
- 
-
-
-
     return (
 
         <div data-aos="fade-up" className="my-20">
-            
+
             <h1 className='font-bold text-2xl mb-16 mt-8 text-blue-700 tracking-[5px]'style={{ fontFamily: "arial" }}>Popular Brands</h1>
             <div  className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 place-items-center gap-12 w-[90%] m-auto cursor-pointer">
             {Brands.map(({item , icon: Icon}) => (
