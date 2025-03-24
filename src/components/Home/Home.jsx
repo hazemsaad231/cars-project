861feb152e46b73bfbe6aca1f27efb773256352a
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-03-25 00:50:38 +0200

    up

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,85 +1,9 @@
-// import One from './part1';
-// import Two from './part2';
-// import Offer from './part3';
-// import Four from './part4'
-// import Six from './part6'
-// import Seven from './part7';
-// import Eight from './part8';
-// import Brand from './part5';
-// import { useEffect } from 'react';
-// import { useLocation, useNavigate } from 'react-router-dom';
-// import { toast, ToastContainer } from 'react-toastify';
-// import Aos from "aos";
-// import "aos/dist/aos.css";
-
-
-
-
-
-
-
-
-// const Home = ()=>{
-
-//     const location = useLocation();
-//     const navigate = useNavigate();
-
-//     useEffect(() => {
-//         Aos.init({ duration: 1000, once: true });
-
-//         const handleScroll = () => {
-//             Aos.refresh();
-//           };
-      
-//           window.addEventListener("scroll", handleScroll);
-//           return () => window.removeEventListener("scroll", handleScroll);
-
-//       }, []);
-
-      
-//     useEffect(() => {
-//         if (location.state?.message) {
-//             toast.success(location.state.message, { autoClose: 2000 }); // عرض الرسالة
-//             navigate(location.pathname, { replace: true }); 
-//         }
-//     }, [location.state, navigate]);
-
-//     return(
-
-// <>
-// <ToastContainer/>
-// <div style={{ fontFamily: "Arial" }}>
-// <One/>
-// <Two/>
-// <Offer/>
-// <Four/>
-// <Brand/>
-// <Six/>
-// <Seven/>
-// <Eight/>
-// </div>
-
-       
-   
-    
-       
-        
-//  </>
-
-
-
-//     )
-// }
-
-
-
-// export default Home;
-
 import { useEffect, Suspense } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import React from "react";
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const One = React.lazy(() => import("./part1"));
 const Two = React.lazy(() => import("./part2"));
@@ -94,11 +18,19 @@ const Home = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    import("aos").then((Aos) => {
-      Aos.default.init({ duration: 1000, once: true });
-    });
-  }, []);
+
+
+    useEffect(() => {
+        Aos.init({ duration: 1000, once: true });
+
+        const handleScroll = () => {
+            Aos.refresh();
+          };
+      
+          window.addEventListener("scroll", handleScroll);
+          return () => window.removeEventListener("scroll", handleScroll);
+
+      }, []);
 
   useEffect(() => {
     if (location.state?.message) {
