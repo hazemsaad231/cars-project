76a643bf4cc1bc1cec985c1680e389b1c35d3a23
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-05-16 20:09:05 +0300

    go

--- src/components/login/image.jsx
+++ src/components/login/image.jsx
@@ -1,16 +1,21 @@
-import newImage from '../../../src/assets/img/new.mp4'
+import newImage from '../../../src/assets/img/new.jpeg'
 
 const Image = () => {
     
 
     return(
         
-<div className='w-full h-screen'>
-    <video autoPlay loop muted disablePictureInPicture playsInline className='w-full h-screen object-cover'>
+<div className='w-full h-screen'> 
+
+    <img src={newImage} alt="" className='w-full h-screen object-center' />
+    {/* <video autoPlay loop muted disablePictureInPicture playsInline className='w-full h-screen object-cover'>
         <source src={newImage} type="video/mp4" />
-    </video>
+    </video> */}
 </div>
+
     )
 }
 
 export default Image
+
+
