50954aa075c92e407baebde117efc88e3c3c7020
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-07-25 01:47:09 +0300

    push

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
+import './i18n';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ContextProvider from './components/context/Context'
-import './i18n'; // عشان يحمل التهيئة قبل ما React يبدأ
 
 
 
