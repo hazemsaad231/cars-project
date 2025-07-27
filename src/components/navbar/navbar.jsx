4b091e9ba331071d853c48a8085cb528fcbf78f5
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-07-27 12:22:32 +0300

    update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -13,6 +13,7 @@ import DialogActions from '@mui/joy/DialogActions';
 import Button from '@mui/joy/Button';
 import Divider from '@mui/joy/Divider';
 import { useTranslation } from 'react-i18next';
+import i18n from '../../i18n'; // أو المسار الصحيح حسب مكان ملف i18n.js
 
 const Navbar = () => {
 
@@ -49,7 +50,7 @@ const handleLogout = () => {
 
 
     // استخدام الترجمة
-    const { t, i18n } = useTranslation();
+    const { t } = useTranslation();
     const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
     setSelect(lng === 'en'); // تحديث حالة select بناءً على اللغة المختارة
